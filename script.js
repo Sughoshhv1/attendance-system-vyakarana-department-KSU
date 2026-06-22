2746d836752f51221ecec4063014598a9240444e
Sughoshhv1 <hvsughosh@gmail.com>
2026-06-22 11:32:48 +0530

    updated password cell and gave a eye button for it

--- script.js
+++ script.js
@@ -389,3 +389,17 @@ async function submitWork() {
     btn.innerText = "Submit";
   }
 }
+function togglePassword() {
+  const passwordInput = document.getElementById("password");
+  const eyeIcon = document.querySelector(".toggle-password");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  }
+}
